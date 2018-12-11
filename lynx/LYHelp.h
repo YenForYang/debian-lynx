8da8e3636e02762364de257054865849b3c20d3a
Richard <richard@yenforyang.com>
2018-12-11 20:30:53 +0100

    first commit

---
+++ lynx/LYHelp.h
@@ -0,0 +1,26 @@
+/*
+ * $LynxId: LYHelp.hin,v 1.4 2008/01/10 23:26:53 tom Exp $
+ * vile:cmode
+ */
+/* definitions for help files */
+
+#ifndef LYHELP_H
+#define LYHELP_H
+
+#define ALT_EDIT_HELP		"keystrokes/alt_edit_help.html.gz"
+#define BASHLIKE_EDIT_HELP	"keystrokes/bashlike_edit_help.html.gz"
+#define COOKIE_JAR_HELP		"Lynx_users_guide.html.gz#Cookies"
+#define CACHE_JAR_HELP		"Lynx_users_guide.html.gz#Cache"
+#define CURRENT_KEYMAP_HELP	"keystrokes/keystroke_help.html.gz"
+#define DIRED_MENU_HELP		"keystrokes/dired_help.html.gz"
+#define EDIT_HELP		"keystrokes/edit_help.html.gz"
+#define DOWNLOAD_OPTIONS_HELP	"Lynx_users_guide.html.gz#RemoteSource"
+#define HISTORY_PAGE_HELP	"keystrokes/history_help.html.gz"
+#define LIST_PAGE_HELP		"keystrokes/follow_help.html.gz"
+#define LYNXCFG_HELP		"lynx.cfg"
+#define OPTIONS_HELP		"keystrokes/option_help.html.gz"
+#define PRINT_OPTIONS_HELP	"keystrokes/print_help.html.gz"
+#define UPLOAD_OPTIONS_HELP	"Lynx_users_guide.html.gz#DirEd"
+#define VISITED_LINKS_HELP	"keystrokes/visited_help.html.gz"
+
+#endif /* LYHELP_H */
